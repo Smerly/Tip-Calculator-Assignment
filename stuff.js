513fd2c43754fd16893376c0635b8f0b1915432d
Smerly <69550699+Smerly@users.noreply.github.com>
2020-09-24 18:46:33 -0700

    Add files via upload

--- stuff.js
+++ stuff.js
@@ -1,38 +1,30 @@
-const billInput = document.querySelector('#bill') {
+const billInput = document.querySelector('#bill-input');
+const tipInput = document.querySelector('#tip-input');
+const numOfPplInput = document.querySelector('#num-of-ppl-input');
+const displayTip = document.querySelector('#display-tip');
+const displayTotal = document.querySelector('#display-total');
 
+billInput.addEventListener('input', handleBill);
+tipInput.addEventListener('input', handleBill);
+numOfPplInput.addEventListener('input', handleBill);
+
+
+
+
+
+function handleBill(e) {
+    const percent = parseFloat(tipInput.value);
+    const bill = parseFloat(billInput.value);
+    const numOfPpl = parseFloat(numOfPplInput.value);
+
+    const tip = bill * percent / 100 / numOfPpl;
+    const total = bill + tip;
+
+    displayTip.innerHTML = tip.toFixed(2);
+    displayTotal.innerHTML = total.toFixed(2);
     
-
-
-
 
 }
 
-const tipInput = document.querySelector('#tip') {
 
-    
-    
-    
-    
-    
-    }
-
-
-
-
-}
-
-const numOfPplInput = document.querySelector('#numOfPpl') {
-
-   
-        
-    
-    
-    
-    
-    }
-
-
-
-
-}
-
+handleBill(e);
